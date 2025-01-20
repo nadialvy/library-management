f45762d3e0f640af8bd15b8d0a2bda9f87818166
nadialvy <5026231007@student.its.ac.id>
2025-01-20 12:35:30 +0700

    feat: crud on books and install air to hot reload

--- handlers/book.go
+++ handlers/book.go
@@ -1,1 +1,70 @@
 package handlers
+
+import (
+	"library-management/database"
+	"library-management/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateBook(c *gin.Context) {
+	var book models.Book
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Create(&book)
+	c.JSON(http.StatusOK, book)
+}
+
+func GetBooks(c *gin.Context) {
+	var books []models.Book
+	database.DB.Find(&books)
+	c.JSON(http.StatusOK, books)
+}
+
+func GetBookByName(c *gin.Context) {
+	var books []models.Book
+	name := c.Param("name")
+
+	if err := database.DB.Where("title ILIKE ?", "%"+name+"%").Find(&books).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if len(books) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, books)
+}
+
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+	if err := database.DB.First(&book, c.Param("id")).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&book); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Save(&book)
+	c.JSON(http.StatusOK, book)
+}
+
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+	if err := database.DB.First(&book, c.Param("id")).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Book not found"})
+		return
+	}
+
+	database.DB.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{"message": "Book deleted"})
+}
--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"library-management/models"
-	"log"
-	"os"
+	"library-management/database"
+	"library-management/handlers"
 
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/gin-gonic/gin"
 )
 
-// define global connection using gorm
-// *gorm.DB is a struct and serve CRUD
-var DB *gorm.DB
-
-func initDatabase() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-	log.Println("Database connection successful")
-
-	// Auto migrate models
-	DB.AutoMigrate(&models.Book{})
-	log.Println("Database connected and migrated")
-}
 
 func main(){
-	initDatabase()
+	database.InitDatabase()
+
+	// setup gin
+	r := gin.Default()
+
+	// Routes
+	r.POST("/books", handlers.CreateBook)
+	r.GET("/books", handlers.GetBooks)
+	r.GET("/books/:name", handlers.GetBookByName)
+	r.PUT("/books/:id", handlers.UpdateBook)
+	r.DELETE("/books/:id", handlers.DeleteBook)
+
+	r.Run(":8080")
 }
